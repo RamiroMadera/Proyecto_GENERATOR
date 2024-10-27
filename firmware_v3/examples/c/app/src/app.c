2cb2b091653e21d4b26558fda1fb7ffca80d94f8
TobiasGI <tobiasgarciaiacovelli@gmail.com>
2024-10-26 21:01:15 -0300

    Aca esta lo que subio rama

--- firmware_v3/examples/c/app/src/app.c
+++ firmware_v3/examples/c/app/src/app.c
@@ -1,77 +1,56 @@
-/*============================================================================
- * Autor:
- * Licencia:
- * Fecha:
- *===========================================================================*/
+#include "../inc/app.h"
 
-// Inlcusiones
+#include "ili9341.h"
+#include "sapi.h"
 
-#include "app.h"         // <= Su propia cabecera
-#include "sapi.h"        // <= Biblioteca sAPI
 
-// FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
-int main( void )
-{
-   // ---------- CONFIGURACIONES ------------------------------
+int main(void) {
 
-   // Inicializar y configurar la plataforma
-   boardConfig();
+    boardConfig();
+    ili9341_gpio_init();
+    ili9341_spi_init();
 
-   // Crear varias variables del tipo booleano
-   bool_t buttonValue = OFF;
-   bool_t ledValue    = OFF;
-   // Crear variable del tipo tick_t para contar tiempo
-   tick_t timeCount   = 0;
+    // Inicializaci�n y configuraci�n del display ILI9341
+    ili9341_desc_ptr_t display;
+    const ili9341_cfg_t display_cfg = {
+        .cs_pin = gpio_cs_pin,
+        .dc_pin = gpio_dc_pin,
+        .rst_pin = gpio_rst_pin,
+        .spi_tx_dma = spi_tx_dma_b,
+        .spi_tx_ready = spi_tx_dma_ready,
+        .orientation = ILI9341_ORIENTATION_HORIZONTAL,
+        .width = 320,
+        .height = 240,
+        .timeout_ms = 10000,
+        .wup_delay_ms = 20,
+        .restart_delay_ms = 20
+    };
 
-   // ---------- REPETIR POR SIEMPRE --------------------------
-   while( TRUE ) {
+    const ili9341_hw_cfg_t hw_cfg = ili9341_get_default_hw_cfg();
 
-      /* Retardo bloqueante durante 100ms */
-      
-      delay( 100 );
-      
-      /* Si pasaron 10 segundos comienza a funcionar el programa que copia las
-         acciones en BOTON al LED. Mientras espera titila el LED.  */
-      
-      timeCount++;      
-      
-      if( timeCount == 100 ){ // 100ms * 100 = 10s
-         
-         while( TRUE ) {
-            
-            /* Si se presiona CIAA_BOARD_BUTTON, enciende el CIAA_BOARD_LED */
+    display = ili9341_init(&display_cfg, &hw_cfg);
+    if (display == NULL) {
+        //Error_Handler();
 
-            // Leer pin conectado al boton.
-            buttonValue = gpioRead( CIAA_BOARD_BUTTON );
-            // Invertir el valor leido, pues lee un 0 (OFF) con boton
-            // presionado y 1 (ON) al liberarla.
-            buttonValue = !buttonValue;
-            // Escribir el valor leido en el LED correspondiente.
-            gpioWrite( CIAA_BOARD_LED, buttonValue );
+        //ojalá no entre acá xd
+    }
 
-            /* Enviar a la salida estandar (UART_DEBUG) el estado del LED */
-            
-            // Leer el estado del pin conectado al led
-            ledValue = gpioRead( CIAA_BOARD_LED );
-            // Chequear si el valor leido es encedido
-            if( ledValue == ON ) {
-               // Si esta encendido mostrar por UART_USB "LED encendido."
-               printf( "LED encendido.\r\n" );
-            } else {
-               // Si esta apagado mostrar por UART_USB "LED apagado."
-               printf( "LED apagado.\r\n" );
-            }
-            delay( 250 );
-            
-         }
-      } else {
-         // Intercambiar el valor de CIAA_BOARD_LED
-         gpioToggle(CIAA_BOARD_LED);
-      }
-   }
+    // C�digo de aplicaci�n
 
-   // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
-   // directamenteno sobre un microcontroladore y no es llamado por ningun
-   // Sistema Operativo, como en el caso de un programa para PC.
-   return 0;
+    // Coordenadas para centrar el cuadrado
+    coord_2d_t square_top_left = {110, 70}; // (320-100)/2, (240-100)/2
+    coord_2d_t square_bottom_right = {210, 170};
+
+    // Coordenadas para el círculo
+    uint16_t center_x = 160; // 320/2
+    uint16_t center_y = 120; // 240/2
+    uint16_t radius = 50;
+
+    ili9341_set_region(display, square_top_left, square_bottom_right);
+    ili9341_fill_region(display, RED);
+
+    //draw_circle(display, center_x, center_y, radius, BLUE);
+    while (1) {
+        // C�digo de la aplicaci�n
+    }
 }
