eee153142a26d17578ff0a0a064f32d340ba2467
RamiroMadera <ramiro.madera@alu.ing.unlp.edu.ar>
2024-11-28 09:12:14 -0300

    patch nuevo poncho

--- firmware_v3/examples/c/app/inc/ili9341_LPC4337_patch.h
+++ firmware_v3/examples/c/app/inc/ili9341_LPC4337_patch.h
@@ -24,8 +24,8 @@ int spi_tx_dma_b(const uint8_t* data, uint32_t len);
 
 /* Cons */
 
-#define CS_PIN      LCD1
-#define DC_PIN      GPIO0
-#define RST_PIN     GPIO2
+#define CS_PIN      GPIO8   //LCD1
+#define DC_PIN      GPIO5   //GPIO0
+#define RST_PIN     GPIO7   //GPIO2
 
 #endif // ILI9341_ILI9341_LPC4337_PATCH_H_
