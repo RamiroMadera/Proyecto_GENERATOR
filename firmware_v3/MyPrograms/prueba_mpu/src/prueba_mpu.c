f4d7b083e50b31c3e16a40cb78b86081ebcdca48
TobiasGI <tobiasgarciaiacovelli@gmail.com>
2024-12-06 13:49:47 -0300

    cambios varios

--- firmware_v3/MyPrograms/prueba_mpu/src/prueba_mpu.c
+++ firmware_v3/MyPrograms/prueba_mpu/src/prueba_mpu.c
@@ -13,30 +13,37 @@ int main(void){
       while(1);
    }
    printf("MPU6050 inicializado correctamente.\r\n\r\n" );
-   float AccelX, AccelY, AccelZ, GyroX, GyroY, GyroZ;
-   float umbralGiroscopio = 5000.0; // Umbral para aceleración radial
-   float umbralAceleracion = 10000.0; // Umbral para aceleración lineal
+   float AccelX, AccelY, AccelZ, GyroX, GyroY, GyroZ, AccelX1, AccelY1, AccelZ1, GyroX1, GyroY1, GyroZ1;
+   float umbralGiroscopio = 4; // Umbral para aceleración radial
+   float umbralAceleracion = 4; // Umbral para aceleración lineal
+   mpu60X0Read();
+   GyroX1 = mpu60X0GetGyroX_rads();
+   GyroY1 = mpu60X0GetGyroY_rads();
+   GyroZ1 = mpu60X0GetGyroZ_rads();
+   AccelX1 = mpu60X0GetAccelX_mss();
+   AccelY1 = mpu60X0GetAccelY_mss();
+   AccelZ1 = mpu60X0GetAccelZ_mss();
    /* ------------- REPETIR POR SIEMPRE ------------- */
    while(TRUE){
 
       //Leer el sensor y guardar en estructura de control
       mpu60X0Read();
 
-      GyroX = mpu60X0GetGyroX_rads();
-      GyroY = mpu60X0GetGyroY_rads();
-      GyroZ = mpu60X0GetGyroZ_rads();
+      GyroX = mpu60X0GetGyroX_rads() - GyroX1;
+      GyroY = mpu60X0GetGyroY_rads() - GyroY1;
+      GyroZ = mpu60X0GetGyroZ_rads() - GyroZ1;
       printf( "Giroscopo:  (%f, %f, %f)   [rad/s]\r\n",GyroX, GyroY, GyroZ);
       // Prender led si se esta girando
-      if ((GyroX > umbralAceleracion || GyroX < -umbralAceleracion) ||
-         (GyroY > umbralAceleracion || GyroY < -umbralAceleracion) ||
-         (GyroZ > umbralAceleracion || GyroZ < -umbralAceleracion)) {
+      if ((GyroX > umbralGiroscopio || GyroX < -umbralGiroscopio) ||
+         (GyroY > umbralGiroscopio || GyroY < -umbralGiroscopio) ||
+         (GyroZ > umbralGiroscopio || GyroZ < -umbralGiroscopio)) {
          gpioWrite(LED1,HIGH);
       } else {
          gpioWrite(LED1,LOW);
       }
-      AccelX = mpu60X0GetAccelX_mss();
-      AccelY = mpu60X0GetAccelY_mss();
-      AccelZ = mpu60X0GetAccelZ_mss();
+      AccelX = mpu60X0GetAccelX_mss() - AccelX1;
+      AccelY = mpu60X0GetAccelY_mss() - AccelY1;
+      AccelZ = mpu60X0GetAccelZ_mss() - AccelZ1;
       printf( "Acelerometro:  (%f, %f, %f)   [m/s2]\r\n",AccelX, AccelY, AccelZ);
       // Prender led si se esta agitando
       if ((AccelX > umbralAceleracion || AccelX < -umbralAceleracion) ||
@@ -46,11 +53,9 @@ int main(void){
       } else {
          gpioWrite(LED2,LOW);
       }
-		printf( "Temperatura:   %f [C]\r\n\r\n",
-              mpu60X0GetTemperature_C()
-            );
+		//printf( "Temperatura:   %f [C]\r\n\r\n",  mpu60X0GetTemperature_C() );
 
-      delay(1000);
+      delay(500);
    }
    return 0 ;
 }
