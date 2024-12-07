ce59c5e302e0be300ac46c1c7fe9846f29a90faf
TobiasGI <tobiasgarciaiacovelli@gmail.com>
2024-12-07 15:18:23 -0300

    Pareceria estar funcionando, faltan pruebas

--- firmware_v3/MyPrograms/prueba_sonido/src/prueba_sonido.c
+++ firmware_v3/MyPrograms/prueba_sonido/src/prueba_sonido.c
@@ -10,44 +10,37 @@
 #include "dados.h"
 
 unsigned short audioData10Bit[DATA_SIZE / 2];
-static uint32_t index = 0;
+static uint32_t i = 0;
 
-// Función para convertir de 16 bits a 10 bits
-void convertTo10Bits(void);
 void playAudio(void);
-
 
 int main(void) {
    bool_t buttonValue = OFF;
+   bool_t ok = OFF;
    /* ------------- INICIALIZACIONES ------------- */
    boardConfig();
    uartConfig(UART_USB, 115200);   // Inicializar UART
    dacConfig(DAC_ENABLE);         // Inicializar DAC
-   convertTo10Bits();
    while(1){
       buttonValue = gpioRead( CIAA_BOARD_BUTTON );
-      if (!buttonValue){
-         playAudio();
-         delayInaccurateUs(63);
+      if (!buttonValue) {
+         ok=TRUE;
+         delay(200);
+      }
+      if (ok){
+         for (int j = 0; j < DATA_SIZE / 2; j++) {
+            dacWrite(DAC, rawData[j]);
+            delayInaccurateUs(125);
+         }
+         ok=OFF;
       }
    }
    return 0;
 }
 
-void convertTo10Bits(void) {
-    for (int i = 0; i < DATA_SIZE / 2; i++) {
-        // Convertir cada par de bytes de 16 bits a 16 bits sin signo
-        unsigned short value16 = (rawData[2 * i] << 8) | rawData[2 * i + 1];
-        
-        // Mapear de 16 bits a 10 bits
-        audioData10Bit[i] = (value16 * 1023) / 65535;  // Proporción lineal
-    }
-}
-
 void playAudio(void) {
-    // Reproduce el valor del arreglo audioData10Bit
-    dacWrite(DAC, audioData10Bit[index]);
-
-    // Actualiza el índice de la muestra
-    index = (index + 1) % (sizeof(audioData10Bit) / sizeof(audioData10Bit[0]));
+   if (i==0) printf("Comenzando \r\n");
+   if (i==(DATA_SIZE)) printf("Fin \r\n\r\n");
+   dacWrite(DAC, rawData[i]);
+   i = (i + 1) % DATA_SIZE;
 }
